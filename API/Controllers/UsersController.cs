bd58940ba0eae3199d2c82118dff22004152f746
Penny <sing067808@gmail.com>
2024-06-15 22:47:43 +0800

    0615

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]// /api/users
+public class UsersController:ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]// /api/users/id
+
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    //使用Id的話，會自動成為主鍵。
+    //或使用[Key]來決定。
+
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
